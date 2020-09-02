273149113e7c531a3e6f8e659ece7f370829da07
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-09-02 16:43:27 -0300

    done

---
+++ node/aula1/app.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const axios = require("axios");
+const { Pessoa } = require("./mod1");
+const mod1 = require("./mod1");
+
+console.log(Pessoa);
+console.log(mod1);
+
+// axios("https://www.otaviomiranda.com.br/files/json/pessoas.json")
+//   .then((response) => console.log(response.data))
+//   .catch((e) => console.log(e));
