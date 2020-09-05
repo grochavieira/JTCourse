20bda077c6dc435c266d698b5d636aa4b9b9a5e1
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-09-05 11:41:09 -0300

    done

---
+++ node/aula2/app.js
@@ -0,0 +1,11 @@
+// const path = require("path");
+
+// console.log(path.resolve(__dirname, "..", ".."));
+
+// console.log(__filename);
+// console.log(__dirname);
+
+const Cachorro = require("./Z/mod2.js");
+
+const c1 = new Cachorro("Dog");
+c1.latir();
