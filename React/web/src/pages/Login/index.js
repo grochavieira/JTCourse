71c7c51fd45c833e050e13a7d0c8957c10600dc1
grochavieira <guirocha.hopeisaba@gmail.com>
2020-11-15 13:01:21 -0300

    .

--- React/web/src/pages/Login/index.js
+++ React/web/src/pages/Login/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
 
+import axios from "../../services/axios";
 import { Container } from "../../styles/GlobalStyles";
 import { Title } from "./styled";
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function getData() {
+      const { data } = await axios.get("/alunos");
+      console.log(data);
+    }
+    getData();
+  }, []);
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: "BOTAO_CLICADO",
+    });
+  }
+
   return (
     <Container>
       <h1>
@@ -12,7 +32,7 @@ export default function Login() {
         <Title isRed={false}>Oe</Title>
       </h1>
       <p>Lorem ipsum dolor sit amet.</p>
-      <button>Enviar</button>
+      <button onClick={handleClick}>Enviar</button>
     </Container>
   );
 }
