7c85a55fad7e0681667736401d232647a84e8371
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-01 10:53:33 -0300

    .

---
+++ TypeScript/TiposBasicos/src/A0021/A0021.ts
@@ -0,0 +1,33 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Guilherme', 'Rocha');
+empresa1.adicionaColaborador(colaborador1);
+console.log(empresa1);
+empresa1.mostrarColaboradores();
