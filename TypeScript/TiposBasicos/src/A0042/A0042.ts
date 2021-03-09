c3983f278be532120765827380f5545af813f316
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-09 13:13:23 -0300

    TypeScript advanced types section done

---
+++ TypeScript/TiposBasicos/src/A0042/A0042.ts
@@ -0,0 +1,18 @@
+type Documento = {
+  titulo: string;
+  texto: string;
+  data?: Date;
+};
+
+const documento: Documento = {
+  titulo: 'O titulo',
+  texto: 'O texto',
+  // data: new Date(),
+};
+
+console.log(documento.data?.toDateString() ?? '1-Ixi, não existe data.');
+console.log(undefined ?? '2-Ixi, não existe data.');
+console.log(null ?? '3-Ixi, não existe data.');
+console.log(false ?? '3-Ixi, não existe data.');
+console.log(0 ?? '4-Ixi, não existe data.');
+console.log('' ?? '5-Ixi, não existe data.');
