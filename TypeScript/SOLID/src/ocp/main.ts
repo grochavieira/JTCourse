a0009be3fda531e6cf21c61e0dc46d817ad8312a
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-21 10:10:51 -0300

    open/closed principle done

---
+++ TypeScript/SOLID/src/ocp/main.ts
@@ -0,0 +1,28 @@
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import {
+  FiftyPercentDiscount,
+  NoDiscount,
+  TenPercentDiscount,
+} from './classes/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+shoppingCart.addItem(new Product('Camiseta', 39.9));
+shoppingCart.addItem(new Product('Caderno', 29.9));
+shoppingCart.addItem(new Product('Lápis', 1.9));
+shoppingCart.clear();
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+order.checkout();
+console.log(order.orderStatus);
