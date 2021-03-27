891947f74f76db394c8822b42b74aeda5e5aac79
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-27 16:24:37 -0300

    initiated tests with jest section

---
+++ TypeScript/Testes/src/services/persistency.spec.ts
@@ -0,0 +1,10 @@
+describe('Testando alguma coisa', () => {
+  it('should return one (IT)', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+  test('should return giorno giovanna (TEST)', () => {
+    const name = 'giorno giovanna';
+    expect(name).toBe('giorno giovanna');
+  });
+});
